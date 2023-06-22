c4590896169081df686b005c9855bb7dd1fbccae
adfasdf <asdf@repurels.com>
2023-06-22 18:29:10 +0900

    feat. 테일윈드 적용

---
+++ src/components/common/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="text-5xl text-center">
+        This is Header
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/layouts/HomeLayout.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    HOMELAYOUT
+</div>
+</template>
+
+<script>
+export default {
+    name: "HomeLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
