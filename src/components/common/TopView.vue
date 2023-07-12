5fd49750739e255e06eeccbb44504fb2bca8d93d
adfasdf <asdf@repurels.com>
2023-07-12 18:47:03 +0900

    - 오늘 날짜(yyyy.mm.dd (요일)) 추가 - 현재 실시간 시간 추가

--- src/components/common/TopView.vue
+++ src/components/common/TopView.vue
@@ -1,36 +1,92 @@
 <template>
     <header class="fixed left-1/2 -translate-x-1/2 w-full border-b bg-white">
-        <h1 class="text-3xl text-center py-8">춤추는 프론트엔드<span class="text-lg">(비열함)</span></h1>
+        <h1 class="text-3xl text-center py-8">
+            춤추는 프론트엔드<span class="text-lg">(비열함)</span>
+        </h1>
 
-        <div class="">
-            <ul class="flex justify-center gap-32">
-                <li class="transition-all"
-                    :class="{ 'border-b-4 border-[#1ABC9C]' : this.$route.path === '/home' }">
-                    <router-link id="home"
-                                 to="/home"
-                                 class="relative inline-block rounded z-10 overflow-hidden px-4 py-1"
-                                 >HOME</router-link>
-                </li>
-                <li class="transition-all"
-                    :class="{ 'border-b-4 border-[#1ABC9C]' : this.$route.path === '/category' }">
-                    <router-link id="category"
-                                 to="/category"
-                                 class="relative inline-block rounded z-10 overflow-hidden px-4 py-1">CATEGORY</router-link>
-                </li>
-                <li class="transition-all"
-                    :class="{ 'border-b-4 border-[#1ABC9C]' : this.$route.path === '/visitor' }">
-                    <router-link id="visitor"
-                                 to="/visitor"
-                                 class="relative inline-block rounded z-10 overflow-hidden px-4 py-1">VISITOR</router-link>
-                </li>
-            </ul>
+        <ul class="flex justify-center gap-32">
+            <li :class="{ 'border-b-4 border-[#1ABC9C]' : this.$route.path === '/home' }">
+                <router-link id="home"
+                             to="/home"
+                             class="relative inline-block rounded z-10 overflow-hidden px-4 py-1"
+                >HOME</router-link>
+            </li>
+            <li :class="{ 'border-b-4 border-[#1ABC9C]' : this.$route.path === '/category' }">
+                <router-link id="category"
+                             to="/category"
+                             class="relative inline-block rounded z-10 overflow-hidden px-4 py-1"
+                >CATEGORY</router-link>
+            </li>
+            <li :class="{ 'border-b-4 border-[#1ABC9C]' : this.$route.path === '/visitor' }">
+                <router-link id="visitor"
+                             to="/visitor"
+                             class="relative inline-block rounded z-10 overflow-hidden px-4 py-1"
+                >VISITOR</router-link>
+            </li>
+        </ul>
+
+        <div class="text-black absolute top-10 right-10">
+            <span v-html="nowYear+'.'+ nowMonth + '.' + nowDay + '('+dayOfWeek+')'"
+                  class="text-black mr-2"></span>
+            <span ref="nowLiveTime"></span>
         </div>
     </header>
 </template>
 
 <script>
 export default {
-    name: "TopView"
+    name: "TopView",
+    data(){
+        return{
+            nowTotalTime: '',
+            nowYear: '',
+            nowMonth: '',
+            nowDay: '',
+            nowHour: '',
+            nowMinute: '',
+            nowSecond: '',
+            nowDate: '',
+            dayOfWeek: '',
+        }
+    },
+    mounted() {
+        this.isNowTime();
+        setInterval(this.isNowTime, 1000);
+    },
+
+    methods: {
+        isNowTime(){
+            this.nowTotalTime = new Date();
+            this.nowYear = this.nowTotalTime.getFullYear();
+            this.nowMonth = this.nowTotalTime.getMonth();
+            this.nowDay = this.nowTotalTime.getDate();
+            this.nowHour = this.nowTotalTime.getHours();
+            this.nowMinute = this.nowTotalTime.getMinutes();
+            this.nowSecond = this.nowTotalTime.getSeconds();
+
+            this.nowMonth = this.nowMonth.toString();
+            this.nowMonth.length === 1 ? this.nowMonth = '0'+this.nowMonth : this.nowMonth;
+
+            let newDay = new Date().toDateString();
+            const getNewDate = days => {
+                const week = ['일','월','화','수','목','금','토'];
+                this.dayOfWeek = week[new Date(days).getDay()];
+                return this.dayOfWeek;
+            }
+
+            getNewDate(newDay);
+
+            this.nowHour = this.nowHour.toString();
+            this.nowMinute = this.nowMinute.toString();
+            this.nowSecond = this.nowSecond.toString();
+
+            this.nowHour.length === 1 ? this.nowHour = '0' + this.nowHour : this.nowHour;
+            this.nowMinute.length === 1 ? this.nowMinute = '0' + this.nowMinute : this.nowMinute;
+            this.nowSecond.length === 1 ? this.nowSecond = '0' + this.nowSecond : this.nowSecond;
+
+            this.$refs.nowLiveTime.innerText = `${this.nowHour}:${this.nowMinute}:${this.nowSecond}`;
+        }
+    }
 }
 </script>
 
@@ -67,6 +123,4 @@ export default {
     color: #fff !important;
     font-weight: bold;
 }
-
-
 </style>
